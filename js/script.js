ad4177f2cd56bc638a664aafbe6c444c70f24bd4
Swed0Z <arrowdaamazonia@gmail.com>
2024-03-23 16:27:02 -0300

    create: criando uma navbar responsiba
    "Eu criei uma barra de navegação para o meu site. Ela deveria funcionar sem problemas, mas estou enfrentando um erro. Parece que o código JavaScript está sendo executado duas vezes, o que está causando comportamentos inesperados na barra de navegação.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const toggleBtn1 = document.querySelector('.toggle_btn');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn1.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+    debugger
+};
